4c13f7fcff3fd2e10c87523ae53e7ff3e64c1c2c
luis <you@example.com>
2026-03-20 10:07:05 -0400

    correção do criar admin

--- criar_admin.py
+++ criar_admin.py
@@ -1,17 +1,28 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docegla.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docegla.settings")
 django.setup()
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("Superusuário criado com sucesso!")
+print("Verificando criação de superusuário...")
+
+if not username or not email or not password:
+    print("Variáveis do superusuário não definidas corretamente.")
 else:
-    print("Superusuário já existe.")
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        print("Superusuário criado com sucesso!")
+    else:
+        print("Superusuário já existe.")
